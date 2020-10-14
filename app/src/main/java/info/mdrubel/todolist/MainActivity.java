9307608965ad80f71136e907b497640a68461339
Md Rubel Rana <rubel.rana@northsouth.edu>
2020-10-15 01:34:01 +0600

    SplashScreen added +++ MainActivity Layout +++

--- app/src/main/java/info/mdrubel/todolist/MainActivity.java
+++ app/src/main/java/info/mdrubel/todolist/MainActivity.java
@@ -1,13 +1,16 @@
 package info.mdrubel.todolist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //noinspection deprecation
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
---
+++ app/src/main/java/info/mdrubel/todolist/SplashScreen.java
@@ -0,0 +1,30 @@
+package info.mdrubel.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //noinspection deprecation
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
