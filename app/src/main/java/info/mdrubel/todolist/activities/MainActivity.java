e2d8852fd57889c984d05c325032265299df7d04
Md Rubel Rana <rubel.rana@northsouth.edu>
2020-10-16 01:10:59 +0600

    AddTask (Database -> MainActivity) +++

--- app/src/main/java/info/mdrubel/todolist/activities/MainActivity.java
+++ app/src/main/java/info/mdrubel/todolist/activities/MainActivity.java
@@ -8,18 +8,28 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import info.mdrubel.todolist.R;
 import info.mdrubel.todolist.activities.CreateNoteActivity;
+import info.mdrubel.todolist.adapters.NotesAdapter;
 import info.mdrubel.todolist.database.NotesDatabase;
 import info.mdrubel.todolist.entities.Note;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    private RecyclerView notesRecyclerView;
+    private ImageView imageAddNoteMain;
+    private List<Note> noteList;
+    private NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageAddNoteMain = findViewById(R.id.image_add_note_main);
+        //  Hooks
+        imageAddNoteMain = findViewById(R.id.image_add_note_main);
+
         imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,11 +50,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //  Hooks
+        notesRecyclerView = findViewById(R.id.notes_recycler_view);
+
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
+
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList);
+        notesRecyclerView.setAdapter(notesAdapter);
+
         getNotes();
     }
 
-    private void getNotes(){
-        class GetNotesTask extends AsyncTask<Void, Void, List<Note>>{
+    private void getNotes() {
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
             @Override
             protected List<Note> doInBackground(Void... voids) {
                 return NotesDatabase.getDatabase(getApplicationContext()).noteDao().getAllNotes();
@@ -51,9 +74,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("MY_NOTES", notes.toString());
+                if (noteList.size() == 0) {
+                    noteList.addAll(notes);
+                    notesAdapter.notifyDataSetChanged();
+                } else {
+                    noteList.add(0, notes.get(0));
+                    notesAdapter.notifyItemInserted(0);
+                }
+                notesRecyclerView.smoothScrollToPosition(0);
             }
         }
         new GetNotesTask().execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes();
+        }
     }
 }
---
+++ app/src/main/java/info/mdrubel/todolist/adapters/NotesAdapter.java
@@ -0,0 +1,69 @@
+package info.mdrubel.todolist.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import info.mdrubel.todolist.R;
+import info.mdrubel.todolist.entities.Note;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
+
+    private List<Note> notes;
+
+    public NotesAdapter(List<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NoteViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.item_container_note, parent, false)
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.setNote(notes.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder {
+        TextView textTitle, textSubtitle, textDateTime;
+
+        NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //  Hooks
+            textTitle = itemView.findViewById(R.id.text_title);
+            textSubtitle = itemView.findViewById(R.id.text_subtitle);
+            textDateTime = itemView.findViewById(R.id.text_date_time);
+        }
+
+        void setNote(Note note) {
+            textTitle.setText(note.getTitle());
+            if (note.getSubtitle().trim().isEmpty()) {
+                textSubtitle.setVisibility((View.GONE));
+            } else {
+                textSubtitle.setText(note.getSubtitle());
+            }
+            textDateTime.setText(note.getDateTime());
+        }
+    }
+}
